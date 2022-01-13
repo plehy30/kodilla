b75412282ffb1acab0417b39f82580bfff6bd773
Tomasz Leczkowski <plehy30@gmail.com>
2022-01-13 23:06:35 +0100

    29>12

--- kodilla/modul_5/car.py
+++ kodilla/modul_5/car.py
@@ -5,13 +5,24 @@ class Car:
         self.top_speed = top_speed
         self.color = color
 
-        self.current_speed=0
+        self._current_speed = 0
+
+    @property
+    def current_speed(self):
+        return self._current_speed
+
+    @current_speed.setter
+    def current_speed(self, value):
+        if value <= self.top_speed:
+            self._current_speed = value
+        else:
+            raise ValueError(f'Value {value} jest wieksze od {self.top_speed}')
 
     def accererate(self, step=10):
-        self.current_speed+=10
+        self._current_speed += step
 
-    def decelerate(self,step=10):
-        self.current_speed+=-10
+    def decelerate(self, step=10):
+        self._current_speed -= step
 
     def __str__(self):
         return f'{self.color} {self.model_name} {self.make}'
@@ -25,9 +36,9 @@ class Car:
 
 mustang = Car(make='Ford', model_name='Mustang', top_speed=120, color="Yellow")
 mustang1 = Car(make='Ford', model_name='Mustang', top_speed=110, color="Yellow")
+mustang.current_speed = 30
 mustang.accererate()
 print(mustang.current_speed)
 print(mustang > mustang1)
-mustang.accererate(50)
+mustang.accererate(60)
 print(mustang.current_speed)
-
--- kodilla/modul_5/zwierzatko.py
+++ kodilla/modul_5/zwierzatko.py
@@ -1,21 +1,22 @@
 class Zwierze():
-    def __init__(self, nazwa, plec, wiek, waga, rodzaj,wzrost):
+    def __init__(self, nazwa, plec, wiek, waga, rodzaj, wzrost):
         self.nazwa = nazwa
         self.plec = plec
         self.wiek = wiek
         self.waga = waga
         self.rodzaj = rodzaj
-        self.wzrost=wzrost
+        self.wzrost = wzrost
+
     @property
     def stosunek(self):
-        return self.waga/self.wzrost
+        return self.waga / self.wzrost
 
 
-animal1 = Zwierze("koń", False, 5, 300, 'ssak',300)
-animal2 = Zwierze("pies", True, 15, 20, 'ssak',50)
-animal3 = Zwierze('kot', False, 7, 4, 'ssak',45)
-animal4 = Zwierze('karp', False, 1, 2, 'ryba',30)
-animal5 = Zwierze('papuga', True, 10, 1, 'ptak',30)
+animal1 = Zwierze("koń", False, 5, 300, 'ssak', 300)
+animal2 = Zwierze("pies", True, 3, 20, 'ssak', 50)
+animal3 = Zwierze('kot', False, 7, 4, 'ssak', 45)
+animal4 = Zwierze('karp', False, 1, 2, 'ryba', 30)
+animal5 = Zwierze('papuga', True, 10, 1, 'ptak', 30)
 animals = [animal1, animal2, animal3, animal4, animal5]
 
 for a in animals:
@@ -25,5 +26,18 @@ animals.sort(key=lambda animal: animal.nazwa)
 print(f"posortowane po nazwie: ")
 for a in animals:
     print(a.nazwa)
-# animals.sort(key=lambda a: a.wiek)
-# print(f"posortowane po wieku: {animals.wiek}")
+
+animals.sort(key=lambda animal: animal.wiek)
+print(f"posortowane po wieku: ")
+for a in animals:
+    print(a.nazwa)
+waga = []
+for a in animals:
+    waga.append(a.waga)
+srednia = sum(waga) / len(animals)
+print(f'Średnia waga zwierząt wynosi {srednia}')
+wiek = []
+for a in animals:
+    if 8 > a.wiek > 3:
+        wiek.append(a.nazwa)
+print(wiek)
