7338dc6462ff65720c177f9f7d2c3bef35d797ac
Tomasz Leczkowski <plehy30@gmail.com>
2022-02-17 16:40:05 +0100

    Merge remote-tracking branch 'origin/master'

--- kodilla/modul_6/wizytowka/app.py
+++ kodilla/modul_6/wizytowka/app.py
@@ -4,23 +4,22 @@ app = Flask(__name__)
 
 
 @app.route('/o_mnie')
-def ja():  # put application's code here
+def ja():
     return render_template("o_mnie.html")
 
 
-@app.route('/kontakt', methods=['GET', 'POST'])
-def kontakt():  # put application's code here
+@app.route('/kontakt',methods=["GET","POST"])
+def kontakt():
 
-    request.method == 'POST'
-    print(f'Otrzymaliśmy wiadomość: {request.form}')
-    return render_template("kontakt.html")
-    # if request.method == 'GET':
-    #     print("We received GET")
-    #     return render_template("kontakt.html")
-    # elif request.method == 'POST':
-    #     print("We received POST")
-    #     print(request.form)
-    #     return redirect("/")
-
-
-
+    # request.method == 'POST'
+    # print(f'Otrzymaliśmy wiadomość: {request.form}')
+    # return render_template("kontakt.html")
+    if request.method == 'GET':
+        print("We received GET")
+        return render_template("kontakt.html")
+    elif request.method == 'POST':
+        print("We received POST")
+        r=request
+        print(dir(request))
+        print(request.form)
+        return redirect("/")
